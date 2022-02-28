f0ec3c3ae7890b8a5c64628c22db6346b70237c2
Nathan Weeks <1800812+nathanweeks@users.noreply.github.com>
2022-02-28 15:58:50 -0500

    Fix type

--- quantify_umifm_from_alignments.py
+++ quantify_umifm_from_alignments.py
@@ -284,7 +284,7 @@ def quant(args):
                 # Update the gene contribs based on the new r0, but on the 'old' g0.
                 # That is why we remove g from g0 after this step only
                 gene_contrib = dict((gi, sum(gene_read_mapping[(gi, r)] for r in r0)) for gi in g0)
-                ambig_partners = (gene for (gene, contrib) in max_contrib_genes if contrib == 0)
+                ambig_partners = tuple(gene for gene in max_contrib_genes if gene_contrib[gene] == 0)
                
         
                 #Ambig partners will often be a 1-element set. That's ok.
